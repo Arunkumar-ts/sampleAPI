aa303d3a9ebc77ce7777a5597817a757f1f8fe60
Default <Default>
2025-05-31 17:08:50 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/hello',(req, res)=>{
+  res.json({
+    message:"Hello world!"
+  });
+});
+
+app.listen(PORT,()=>{
+    console.log(`server is running on http://localhost:${PORT}`);
+})
