989836dfcf5893370183aa37233d70de18b6b621
Arunkumar TS <tsarunkumar2005pc@gmail.com>
2025-07-08 23:06:14 +0530

    cors update

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const verifyToken = require("./middleware/verifyToken");
 const app = express();
 const PORT = 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true, 
+}));
+
 app.use(express.json());
 
 app.use('/todos', verifyToken, todoRoutes);
